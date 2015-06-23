1b0e29b007f346714f41a1cb7932a9bacf7b24a9
Martin Jahn <martin.jahn@fintura.de>
2015-06-23 19:21:01 +0200

    added first version of NetworkService as main layer between APP and Backend

--- public/modules/services/NetworkService.js
+++ public/modules/services/NetworkService.js
@@ -1,9 +1,72 @@
-angular.module('AntiguasIslas').service('NetworkService', function () {
-	var oNetworkService = {};
+angular.module('ngfin').service('NetworkService', [$http, $q, function ($http, $q) {
+    function errorHandler (oError) {
+        /*
+            if (typeof oError === 'string') {
+                // do something
+            }
+*/
+        return oError;
+    }
 
-	oNetworkService.get = function(sUrl) {
+    return {
+        post: function (sUrl, oData) {
+            var oPromise = $q.defer();
 
-	};
+            oPromise.notify('about to call the route ' + sUrl);
 
-	return oNetworkService;
-});
+            $http.post(sUrl, oData).success(function (oRespone) {
+                oPromise.resolve(oResponse);
+            }).errror(function (oError) {
+                oError = errorHandler(oError);
+                oPromise.reject(oError);
+            });
+
+            return oPromise.promise;
+        },
+
+        get: function (sUrl, oData) {
+            var oPromise = $q.defer();
+
+            oPromise.notify('about to call the route GET ' + sUrl);
+
+            $http.get(sUrl, oData).success(function (oRespone) {
+                oPromise.resolve(oResponse);
+            }).errror(function (oError) {
+                oError = errorHandler(oError);
+                oPromise.reject(oError);
+            });
+
+            return oPromise.promise;
+        },
+
+        put: function (sUrl, oData) {
+            var oPromise = $q.defer();
+
+            oPromise.notify('about to call the route PUT ' + sUrl);
+
+            $http.put(sUrl, oData).success(function (oRespone) {
+                oPromise.resolve(oResponse);
+            }).errror(function (oError) {
+                oError = errorHandler(oError);
+                oPromise.reject(oError);
+            });
+
+            return oPromise.promise;
+        },
+
+        delete: function (sUrl) {
+            var oPromise = $q.defer();
+
+            oPromise.notify('about to call the route DELETE ' + sUrl);
+
+            $http['delete'](sUrl).success(function (oRespone) {
+                oPromise.resolve(oResponse);
+            }).errror(function (oError) {
+                oError = errorHandler(oError);
+                oPromise.reject(oError);
+            });
+
+            return oPromise.promise;
+        }
+    };
+}]);
